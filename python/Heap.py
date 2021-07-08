e91e0140d33a95139916c0425e5d84e68a0412c0
Pranshu Goyal <pranshugoyal@ymail.com>
2021-07-07 23:14:36 -0700

    Remove binary search solution of a problem from Heap.py

--- python/Heap.py
+++ python/Heap.py
@@ -370,26 +370,3 @@ def kthSmallestMatrix(matrix, k):
 
     return h[0][0]
 
-def kthSmallestMatrixSaddleBackSearch(matrix, k):
-    def countLessThanK(n, k):
-        r, c = 0, n-1
-        count = 0
-        while r < n and c >= 0:
-            if matrix[r][c] <= k:
-                count += c+1
-                r += 1
-            else:
-                c -=1
-        return count
-
-    #Binary search for ans
-    n = len(matrix)
-    lo, hi = matrix[0][0], matrix[-1][-1]
-    while lo < hi:
-        mid = lo + (hi-lo)//2
-        count = countLessThanK(n, mid)
-        if count < k:
-            lo = mid + 1
-        else:
-            hi = mid
-    return lo
