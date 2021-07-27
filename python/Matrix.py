6d8f002c6c86b85f2294c64fd32e3fcb5717d1ef
Pranshu Goyal <pranshugoyal@ymail.com>
2021-07-26 21:21:36 -0700

    Added matrix rotation problem

--- python/Matrix.py
+++ python/Matrix.py
@@ -22,3 +22,15 @@ def findMatrixPair(M, n):
                 res = maxM[r+1][c+1] - M[r][c]
             maxM[r][c] = max(M[r][c], maxM[r+1][c], maxM[r][c+1])
     return res
+
+#https://www.geeksforgeeks.org/rotate-a-matrix-by-90-degree-in-clockwise-direction-without-using-any-extra-space/
+def rotateMatrixAnticlockwise(M):
+    n = len(M)
+    for x in range(n//2):
+        for y in range(x, n-x-1):
+            temp = M[x][y]
+            M[x][y] = M[y][n-1-x]
+            M[y][n-1-x] = M[n-1-x][n-1-y]
+            M[n-1-x][n-1-y] = M[n-1-y][x]
+            M[n-1-y][x] = temp
+    return M
