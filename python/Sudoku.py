f7381d12b815c85431a472797ffa5864a11de633
Pranshu Goyal <pranshugoyal@ymail.com>
2021-08-21 12:57:33 -0700

    Organised Sudoku code as methods in Sudoku class

--- python/Sudoku.py
+++ python/Sudoku.py
@@ -23,150 +23,126 @@ class Cell:
         if self.val is not None:
             return False
         else:
+            oldCount = len(self.pos)
             self.pos -= taken
             if len(self.pos) == 1:
                 self.val = list(self.pos)[0]
-                return True
-            else:
-                return False
+            return len(self.pos) != oldCount
 
-def solveSudoku(sudoku):
-    lastUpdate = True
-    while lastUpdate:
-        lastUpdate = updatePass(sudoku)
+class Sudoku:
 
-    if fillCount(sudoku) < 81:
-        return backtrackSolve(sudoku)
-    else:
-        return sudoku
+    def __init__(self, data):
+        self.data = data
 
-def backtrackSolve(sudoku):
-    def chooseNext(new):
-        minpos = 9
-        chosen = None
+    @staticmethod
+    def readSudoku():
+        sudoku = []
         for row in range(9):
+            s = "Enter row " + str(row+1) + " : "
+            data = input(s).split()
+            sudoku.append([])
+            for val in data:
+                if val == "x":
+                    sudoku[-1].append(Cell())
+                else:
+                    sudoku[-1].append(Cell(int(val)))
+        return Sudoku(sudoku)
+
+    def __str__(self):
+        repr = ""
+        for row in self.data:
+            repr += str([str(cell) for cell in row]) + "\n"
+        repr += "Filled: " + str(self.fillCount())
+        return repr
+
+    def __copy__(self):
+        new = []
+        for row in self:
+            new.append([cell.copy() for cell in row])
+        return Sudoku(new)
+
+    def fillCount(self):
+        count = 0
+        for row in self.data:
+            for cell in row:
+                if cell.val is not None:
+                    count += 1
+        return count
+
+    def isValid(self):
+        def rowCheck():
+            for row in self.data:
+                values = set()
+                for cell in row:
+                    if cell.val not in values:
+                        values.add(cell.val)
+                    else:
+                        return False
+            return True
+
+        def columnCheck():
             for column in range(9):
-                lp = len(new[row][column].pos)
-                if lp >= 2 and lp < minpos:
-                    minpos = lp
-                    chosen = (row, column)
-            if minpos == 2:
-                return chosen
-        return chosen
+                values = set()
+                for row in range(9):
+                    val = self.data[row][column].val 
+                    if val not in values:
+                        values.add(val)
+                    else:
+                        return False
+            return True
 
-    new = copy(sudoku)
-    while fillCount(new) < 81:
-        r,c = chooseNext(new)
-        new[r][c].val = new[r][c].pos.pop()
-        testVal = new[r][c].val
-        new = solveSudoku(new)
-        if not isValid(new):
-            sudoku[r][c].pos.remove(testVal)
-            new = copy(sudoku)
+        def blockCheck():
+            for r, c in Sudoku.blockGenerator(0, 0):
+                values = set()
+                for i, j in Sudoku.blockGenerator(r, c):
+                    val = self.data[i][j].val
+                    if val not in values:
+                        values.add(val)
+                    else:
+                        return False
+            return True
 
-def printSudoku(sudoku):
-    for row in sudoku:
-        print([str(cell) for cell in row])
-    print("Filled:", fillCount(sudoku), "\n")
+        return rowCheck() and columnCheck() and blockCheck()
 
-def copy(sudoku):
-    new = []
-    for row in sudoku:
-        new.append([cell.copy() for cell in row])
-    return new
+    @staticmethod
+    def blockGenerator(r, c):
+        for i in range(r*3, (r+1)*3):
+            for j in range(c*3, (c+1)*3):
+                yield i, j
 
-def fillCount(sudoku):
-    count = 0
-    for row in sudoku:
-        for cell in row:
-            if cell.val is not None:
-                count += 1
-    return count
+    def updatePass(self):
+        sudoku = self.data
+        def updateRows():
+            updates = False
+            for row in sudoku:
+                taken = set([cell.val for cell in row])
+                for cell in row:
+                    updates = cell.removePos(taken) or updates 
+            return updates
 
-def updatePass(sudoku):
-    def updateRows():
+        def updateColumns():
+            updates = False
+            for c in range(9):
+                column = [sudoku[row][c] for row in range(9)]
+                taken = set([cell.val for cell in column])
+                for cell in column:
+                    updates = cell.removePos(taken) or updates 
+            return updates
+
+        def updateBlocks():
+            updates = False
+            for (br, bc) in Sudoku.blockGenerator(0,0):
+                block = [sudoku[r][c] for r,c in Sudoku.blockGenerator(br, bc)]
+                taken = set([cell.val for cell in block])
+                for cell in block:
+                    updates = cell.removePos(taken) or updates 
+            return updates
+
         updates = False
-        for row in sudoku:
-            taken = set([cell.val for cell in row])
-            for cell in row:
-                updates = cell.removePos(taken) or updates 
+        updates = updates or updateRows()
+        updates = updates or updateColumns()
+        updates = updates or updateBlocks()
         return updates
-
-    def updateColumns():
-        updates = False
-        for c in range(9):
-            column = [sudoku[row][c] for row in range(9)]
-            taken = set([cell.val for cell in column])
-            for cell in column:
-                updates = cell.removePos(taken) or updates 
-        return updates
-
-    def updateBlocks():
-        updates = False
-        for (br, bc) in blockGenerator(0,0):
-            block = [sudoku[r][c] for r,c in blockGenerator(br, bc)]
-            taken = set([cell.val for cell in block])
-            for cell in block:
-                updates = cell.removePos(taken) or updates 
-        return updates
-
-    updates = False
-    updates = updates or updateRows()
-    updates = updates or updateColumns()
-    updates = updates or updateBlocks()
-    return updates
-
-def isValid(sudoku):
-    def rowCheck():
-        for row in sudoku:
-            values = set()
-            for cell in row:
-                if cell.val not in values:
-                    values.add(cell.val)
-                else:
-                    return False
-        return True
-
-    def columnCheck():
-        for column in range(9):
-            values = set()
-            for row in range(9):
-                val = sudoku[row][column].val 
-                if val not in values:
-                    values.add(val)
-                else:
-                    return False
-
-    def blockCheck():
-        for r, c in blockGenerator(0, 0):
-            values = set()
-            for i, j in blockGenerator(r, c):
-                val = sudoku[i][j].val
-                if val not in values:
-                    values.add(val)
-                else:
-                    return False
-        return True
-
-    return rowCheck() and columnCheck() and blockCheck()
-
-def blockGenerator(r, c):
-    for i in range(r*3, (r+1)*3):
-        for j in range(c*3, (c+1)*3):
-            yield i, j
-
-def readSudoku():
-    sudoku = []
-    for row in range(9):
-        s = "Enter row " + str(row+1) + " : "
-        data = input(s).split()
-        sudoku.append([])
-        for val in data:
-            if val == "x":
-                sudoku[-1].append(Cell())
-            else:
-                sudoku[-1].append(Cell(int(val)))
-    return sudoku
 
 def testSudoku():
     sudoku = []
@@ -185,8 +161,8 @@ def testSudoku():
     for data in arr:
         sudoku.append([])
         for val in data:
-            if val == "x":
+            if val == "x" or val == ".":
                 sudoku[-1].append(Cell())
             else:
                 sudoku[-1].append(Cell(int(val)))
-    return sudoku
+    return Sudoku(sudoku)
